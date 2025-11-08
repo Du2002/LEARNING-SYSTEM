3203ef198c2e827298e2c1e2ad8f98d7a601e0f2
Du2002 <167920074+Du2002@users.noreply.github.com>
2025-11-08 15:18:50 +0530

    initial stage

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import "@/styles/globals.css";
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+ 
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+export default function App({ Component, pageProps } ) {
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
